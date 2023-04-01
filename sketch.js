1ddcf07d3dd4d552c17d07248d0b5901eedc664e
addha <85791287+addha@users.noreply.github.com>
2023-03-31 21:59:36 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+//VAriables Globales
+// Estados del juego      
+
+function preload(){
+
+//carga imagen  de fondo  
+
+//carga animacion  
+//cargar imagen obstaculos
+
+//carga imagenes de game over y restart
+
+
+
+}
+
+function setup(){
+
+  createCanvas(400,400)
+// Imagen de fondo
+
+
+// Creando los terrenos inferior y superior
+
+
+
+      
+// Creando el globo
+
+// Inicializando los grupos
+
+
+// Creando los sprites de fin del juego y reiniciar
+
+}
+
+function draw() {
+  
+  background("black");  
+
+// Hacer que el globo aerostático salte
+// Agregando graveda
+// Generando obstáculos inferiores y superiores
+//condition for END state
+
+
+  
+}
+
+function reset()
+{
+  //reinicia a Play  la varible GameOver
+
+  //ocultar sprites
+
+  //destruye grupos 
+
+}
+
+
+function spawnObstaclesTop() 
+{
+
+// Posiciones "y" aleatorias para los obstaculos superiores
+
+// Generando obstáculos superiores aleatorios 
+
+ // Asignando lifetime a la variables 
+}
+
+function spawnObstaclesBottom() 
+{
+   // Generar obstáculos inferiores aleatorios 
+
+     // Asignar lifetime a la variable
+}
